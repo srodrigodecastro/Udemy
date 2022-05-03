c1ae9e0c0b5fbf4d066d788de088619394e98134
Rodrigo de Castro Souza <103949333+srodrigodecastro@users.noreply.github.com>
2022-05-03 09:52:11 -0300

    Entendendo sintaxes, sentenças e blocos em JS

---
+++ inicio/comentario.js
@@ -0,0 +1,26 @@
+//console.log seve para exibir algo na tela do PC! TUDO O QUE ESTA APÓS AS BARRAS NA LINHA É COMENTÁRIO!
+console.log('Passo #01');
+console.log('Passo #02'); // Da para comentar também após um código em uma linha.
+//console.log('Passo #03');- Ou até mesmo transformar o código em comentário colocando as barras no início!
+console.log('Passo #04 pois o Passo #03 virou um comentário neste exercício');
+
+//Outra forma de utilizar comentários é com comentários em várias linhas, e para isso faremos da seguinte forma:
+console.log('Usando /**/ Exemplo:')
+/*
+Esse é um
+comentário de
+multiplas linhas
+conforme citado nas linhas acima!
+*/
+console.log('Aqui a sentença volta ao normal')
+
+//Por uma questão de estilo podem existir comentários com vários * da seguinte forma:
+
+/*
+ *Aparecendo em cada
+ *linha um asterístico
+ * Assim o próprio programa
+ * entende que você quer organizar 
+ * seus comentários com o * no início
+ * sempre criando um novo automáticamente!
+*/
--- inicio/sentenca_2.js
+++ inicio/sentenca_2.js
@@ -1,3 +1,3 @@
-console.log('Bom dia!');
-console.log('Boa tarde!');
-console.log('Boa noite!');
+console.log('Passo #01');
+console.log('Passo #02');
+console.log('Passo #03');
